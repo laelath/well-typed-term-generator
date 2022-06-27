1fceed423e4d97f92b133b3cb80f83521972092c
Benjamin Quiring <bq2367@gmail.com>
2022-06-26 21:21:53 -0500

    updates

--- lib/exp.ml
+++ lib/exp.ml
@@ -26,8 +26,14 @@ module DataTy = Key.Make (struct let x="d" end)
 
 (* type datatype *)
 type ty = (* TyArrow of ty_label list * ty_label *)
-        | TyArrowExt of ty_params_label * ty_label
-        | TyData of DataTy.t * (ty_label list)
+  | TyArrowExt of {
+      ty_params : ty_params_label;
+      ty_im : ty_label;
+    }
+  | TyData of {
+      dcon_ty : DataTy.t;
+      arg_tys : ty_label list;
+    }
      (* | TyVar of ty_var *)
 type ty_node = TyNode of {ty : ty}
 
@@ -129,17 +135,129 @@ type program = {
     params_extvar : params_label -> extvar;
 
     (* arguments operations *)
-    get_args : args_label -> var list;
-    new_args : extvar -> args_label;
+    new_args : extvar -> exp_label -> args_label;
+    get_args : args_label -> exp_label list;
     add_arg : args_label -> exp_label -> unit;
     (* the node that contains this args label *)
-    args_prev : args_label -> exp_label;
+    args_parent : args_label -> exp_label;
     (* all args labels that are associated with the given extvar *)
     extvar_args : extvar -> args_label list;
     args_extvar : args_label -> extvar;
 
   }
 
+let make_program ty =
+  let exp_tbl : exp_node ExpLabel.Tbl.t = ExpLabel.Tbl.create 100  in
+  let ty_tbl : ty_node TypeLabel.Tbl.t = TypeLabel.Tbl.create 100 in
+  let ty_params_tbl : (ty_label list) TyParamsLabel.Tbl.t = TyParamsLabel.Tbl.create 100 in
+  let params_tbl : (var list) ParamsLabel.Tbl.t = ParamsLabel.Tbl.create 100 in
+  let args_tbl : (exp_label list) ArgsLabel.Tbl.t = ArgsLabel.Tbl.create 100 in
+  let extvar_ty_params_tbl : (ty_params_label list) ExtVar.Tbl.t = ExtVar.Tbl.create 100 in
+  let extvar_params_tbl : (params_label list) ExtVar.Tbl.t = ExtVar.Tbl.create 100 in
+  let extvar_args_tbl : (args_label list) ExtVar.Tbl.t = ExtVar.Tbl.create 100 in
+  let args_parent_tbl : exp_label ArgsLabel.Tbl.t = ArgsLabel.Tbl.create 100 in
+  let ty_params_extvar_tbl : extvar TyParamsLabel.Tbl.t = TyParamsLabel.Tbl.create 100 in
+  let params_extvar_tbl : extvar ParamsLabel.Tbl.t = ParamsLabel.Tbl.create 100 in
+  let args_extvar_tbl : extvar ArgsLabel.Tbl.t = ArgsLabel.Tbl.create 100 in
+
+  let new_var () = Var.make() in
+  let new_extvar () = ExtVar.make () in
+
+  let new_ty node =
+    let lab = TypeLabel.make() in
+    let () = TypeLabel.Tbl.add ty_tbl lab node in
+    lab in
+  let get_ty lab = TypeLabel.Tbl.find ty_tbl lab in
+
+  let new_exp node =
+    let lab = ExpLabel.make() in
+    let () = ExpLabel.Tbl.add exp_tbl lab node in
+    lab in
+  let get_exp lab = ExpLabel.Tbl.find exp_tbl lab in
+  let set_exp lab node = ExpLabel.Tbl.replace exp_tbl lab node in
+
+  let new_ty_params extvar =
+    let lab = TyParamsLabel.make() in
+    let () = TyParamsLabel.Tbl.add ty_params_tbl lab [] in
+    let () = ExtVar.Tbl.replace extvar_ty_params_tbl extvar
+                                (lab :: (ExtVar.Tbl.find extvar_ty_params_tbl extvar)) in
+    let () = TyParamsLabel.Tbl.add ty_params_extvar_tbl lab extvar in
+    lab in
+  let get_ty_params lab = TyParamsLabel.Tbl.find ty_params_tbl lab in
+  let add_ty_param lab ty = 
+    let () = TyParamsLabel.Tbl.replace ty_params_tbl lab
+                                       (ty :: (TyParamsLabel.Tbl.find ty_params_tbl lab)) in
+    () in
+  let extvar_ty_params extvar = ExtVar.Tbl.find extvar_ty_params_tbl extvar in
+  let ty_params_extvar lab = TyParamsLabel.Tbl.find ty_params_extvar_tbl lab in
+
+  let new_params extvar =
+    let lab = ParamsLabel.make() in
+    let () = ParamsLabel.Tbl.add params_tbl lab [] in
+    let () = ExtVar.Tbl.replace extvar_params_tbl extvar
+                                (lab :: (ExtVar.Tbl.find extvar_params_tbl extvar)) in
+    let () = ParamsLabel.Tbl.add params_extvar_tbl lab extvar in
+    lab in
+  let get_params lab = ParamsLabel.Tbl.find params_tbl lab in
+  let add_param lab var = 
+    let () = ParamsLabel.Tbl.replace params_tbl lab
+                                       (var :: (ParamsLabel.Tbl.find params_tbl lab)) in
+    () in
+  let extvar_params extvar = ExtVar.Tbl.find extvar_params_tbl extvar in
+  let params_extvar lab = ParamsLabel.Tbl.find params_extvar_tbl lab in
+  
+
+  let new_args extvar parent =
+    let lab = ArgsLabel.make() in
+    let () = ArgsLabel.Tbl.add args_tbl lab [] in
+    let () = ExtVar.Tbl.replace extvar_args_tbl extvar
+                                (lab :: (ExtVar.Tbl.find extvar_args_tbl extvar)) in
+    let () = ArgsLabel.Tbl.add args_extvar_tbl lab extvar in
+    let () = ArgsLabel.Tbl.add args_parent_tbl lab parent in
+    lab in
+  let get_args lab = ArgsLabel.Tbl.find args_tbl lab in
+  let add_arg lab node = 
+    let () = ArgsLabel.Tbl.replace args_tbl lab
+                                       (node :: (ArgsLabel.Tbl.find args_tbl lab)) in
+    () in
+  let extvar_args extvar = ExtVar.Tbl.find extvar_args_tbl extvar in
+  let args_extvar lab = ArgsLabel.Tbl.find args_extvar_tbl lab in
+  let args_parent lab = ArgsLabel.Tbl.find args_parent_tbl lab in
+  
+  let head = new_exp (ExpNode {exp=Hole; ty=new_ty (TyNode {ty=ty}); prev=None}) in
+
+  {
+    head = head;
+
+    new_var = new_var;
+    new_extvar = new_extvar;
+    new_ty = new_ty;
+    get_ty = get_ty;
+
+    new_exp = new_exp;
+    get_exp = get_exp;
+    set_exp = set_exp;
+
+    new_ty_params = new_ty_params;
+    get_ty_params = get_ty_params;
+    add_ty_param = add_ty_param;
+    extvar_ty_params = extvar_ty_params;
+    ty_params_extvar = ty_params_extvar;
+
+    new_params = new_params;
+    get_params = get_params;
+    add_param = add_param;
+    extvar_params = extvar_params;
+    params_extvar = params_extvar;
+
+    new_args = new_args;
+    get_args = get_args;
+    add_arg = add_arg;
+    args_parent = args_parent;
+    extvar_args = extvar_args;
+    args_extvar = args_extvar;
+
+  }
 
 
 (* check that the prev pointers are correct, and that each node points to itself *)
--- lib/not_useless.ml
+++ lib/not_useless.ml
@@ -10,14 +10,15 @@
  *)
 
 let choose (lst : 'a list) : 'a = List.hd lst
+let choose_split (lst : 'a list) : 'a * ('a list) = List.hd lst, List.tl lst
 
 type worklist = {
     pop : unit -> Exp.exp_label option;
     add : Exp.exp_label -> unit;
   }
 type state = {
-    program : Exp.program;
     worklist : worklist;
+    mutable size : int;
   }
 
 exception InternalError of string
@@ -32,32 +33,39 @@ let assert_is_hole (Exp.ExpNode node) =
 (* Implements the rule:
    E ~> E{alpha + tau} 
  *)
-let extend_extvar st (extvar : Exp.extvar) (ext_ty : Exp.ty_label) = 
+let extend_extvar (prog : Exp.program) (extvar : Exp.extvar) (ext_ty : Exp.ty_label) = 
   let extend : 'a 'b . Exp.extvar -> (Exp.extvar -> 'a list) -> ('a -> unit) -> unit = 
     fun ext get add ->
     let lst = get ext in let handle_elt elt = add elt in
     List.iter handle_elt lst in
 
-  let () = extend extvar st.program.extvar_ty_params 
-                  (fun ty_params -> st.program.add_ty_param ty_params ext_ty) in
-  let () = extend extvar st.program.extvar_params
-                  (fun param -> st.program.add_param param (st.program.new_var())) in
-  let () = extend extvar st.program.extvar_args 
+  let () = extend extvar prog.extvar_ty_params 
+                  (fun ty_params -> prog.add_ty_param ty_params ext_ty) in
+  let () = extend extvar prog.extvar_params
+                  (fun param -> prog.add_param param (prog.new_var())) in
+  let () = extend extvar prog.extvar_args 
                   (fun arg -> let node = Exp.ExpNode {exp = Exp.Hole; 
-                                                   ty = ext_ty; 
-                                                   prev = Some (st.program.args_prev arg)} in
-                              st.program.add_arg arg (st.program.new_exp node)) in
+                                                      ty = ext_ty; 
+                                                      prev = Some (prog.args_parent arg)} in
+                              prog.add_arg arg (prog.new_exp node)) in
   ()
 
 (* takes E[e] and finds all lambdas i such that E_1[lambda_i xs . E_2[e]] *)
-let rec find_enclosing_lambdas st (e : Exp.exp_label) acc : (Exp.params_label list) = 
-  let Exp.ExpNode node = st.program.get_exp e in
+let rec find_enclosing_lambdas (prog : Exp.program) (e : Exp.exp_label) acc : (Exp.params_label list) = 
+  let Exp.ExpNode node = prog.get_exp e in
   let acc = match node.exp with
     | Lambda lam -> lam.params :: acc
     | _ -> acc in
   match node.prev with
   | None -> acc
-  | Some e -> find_enclosing_lambdas st e acc
+  | Some e -> find_enclosing_lambdas prog e acc
+
+
+(*
+  TRANSITIONS 
+ *)
+
+exception BadTransition
 
 (* Implements the rule:
    E_1[lambda_i xs alpha . E_2[<>]] ~> 
@@ -70,47 +78,88 @@ let rec find_enclosing_lambdas st (e : Exp.exp_label) acc : (Exp.params_label li
    E_1{alpha + tau}[lambda_i (x::xs) alpha . E_2{alpha + tau}[x]]
 
  *)
-let not_useless_rule st (e : Exp.exp_label) =
-  let Exp.ExpNode {ty=ty; prev=prev; _} = st.program.get_exp e in
+let not_useless_rule (_ : state) (prog : Exp.program) (e : Exp.exp_label) =
+  let Exp.ExpNode {ty=ty; prev=prev; _} = prog.get_exp e in
   (* TODO: filter this by ty, don't want to cause circularities? *)
-  let params = find_enclosing_lambdas st e [] in
+  let params = find_enclosing_lambdas prog e [] in
   let param = choose params in
-  let extvar = st.program.params_extvar param in
-  let () = extend_extvar st extvar ty in
+  let extvar = prog.params_extvar param in
+  let () = extend_extvar prog extvar ty in
 
-  let x = List.hd (st.program.get_params param) in
-  let () = st.program.set_exp e (Exp.ExpNode {exp=Var {var=x}; ty=ty; prev=prev}) in
+  let x = List.hd (prog.get_params param) in
+  let () = prog.set_exp e (Exp.ExpNode {exp=Var {var=x}; ty=ty; prev=prev}) in
   ()
 
 (* Implements the rule:
    E[<>] ~> E[call <> alpha] where alpha is fresh
  *)
-let create_ext_function_call st (e : Exp.exp_label) =
-  let Exp.ExpNode {ty=ty; prev=prev; _} = st.program.get_exp e in
-  let extvar = st.program.new_extvar() in
-  let f_ty_node = Exp.TyNode {ty = TyArrowExt (st.program.new_ty_params extvar, ty)} in
-  let f_ty = st.program.new_ty f_ty_node in
-  let f = st.program.new_exp (Exp.ExpNode {exp=Hole; ty=f_ty; prev=Some e}) in
-  let args = st.program.new_args extvar in
-  let () = st.program.set_exp e (Exp.ExpNode {exp = Call {func=f; args=args}; ty=ty; prev=prev}) in
+let create_ext_function_call (st : state) (prog : Exp.program) (e : Exp.exp_label) =
+  let Exp.ExpNode {ty=ty; prev=prev; _} = prog.get_exp e in
+  let extvar = prog.new_extvar() in
+  let f_ty = prog.new_ty (Exp.TyNode {ty = Exp.TyArrowExt {ty_params=prog.new_ty_params extvar; ty_im=ty}}) in
+  let f = prog.new_exp (Exp.ExpNode {exp=Hole; ty=f_ty; prev=Some e}) in
+  let args = prog.new_args extvar e in
+  let () = prog.set_exp e (Exp.ExpNode {exp = Call {func=f; args=args}; ty=ty; prev=prev}) in
   let () = st.worklist.add f in
   ()
 
-let generate_exp (st : state) (e : Exp.exp_label) : state =
-  let node = st.program.get_exp e in
+(* Implements the rule:
+   E[<>] ~> E[lambda xs alpha . <>]
+ *)
+let create_ext_lambda (st : state) (prog : Exp.program) (e : Exp.exp_label) = 
+  let Exp.ExpNode {ty=ty; prev=prev; _} = prog.get_exp e in
+  match prog.get_ty ty with
+  | Exp.TyNode {ty = Exp.TyArrowExt {ty_params=ty_params; ty_im=ty_im}} -> 
+     let extvar = prog.ty_params_extvar ty_params in
+     let params_label = prog.new_params extvar in
+     let xs = List.map (fun _ -> prog.new_var()) (prog.get_ty_params ty_params) in
+     let () = List.iter (prog.add_param params_label) xs in
+     let body = prog.new_exp (Exp.ExpNode {exp=Exp.Hole; ty=ty_im; prev=Some e}) in
+     let () = prog.set_exp e (Exp.ExpNode {exp=Exp.Lambda {params=params_label; body=body}; ty=ty; prev=prev}) in
+     let () = st.worklist.add body in
+     ()
+  | _ -> raise BadTransition
+
+(* TODO: need to do environment management before this, or something *)
+(* Implements the rule:
+   E[<>] ~> E[x]
+ *)
+
+(* Implements the rule:
+   E[<>] ~> E[dcon <> ... <>]
+ *)
+
+(* Implements the rule *)
+
+
+
+(* 
+   MAIN LOOP
+ *) 
+
+let generate_exp (st : state) (prog : Exp.program) (e : Exp.exp_label) =
+  let node = prog.get_exp e in
   let () = assert_is_hole node in
-  st
+  let transitions = [not_useless_rule; create_ext_function_call; create_ext_lambda] in
+  let rec lp ts =
+    let t, ts = choose_split ts in
+    try t st prog e with
+    | BadTransition -> lp ts in
+  let () = lp transitions in
+  ()
 
-let generate (st : state) : state option =
+let generate (st : state) (prog : Exp.program) : bool =
   match st.worklist.pop () with
-  | None -> None
-  | Some e -> Some (generate_exp st e)
+  | None -> false
+  | Some e -> (generate_exp st prog e; true)
 
 
-let generate_fp (st : state) : state =
-  match generate st with
-  | None -> st
-  | Some st' -> st'
+let generate_fp (st : state) (prog : Exp.program) : unit =
+  let rec lp () = 
+    match generate st prog with
+    | false -> ()
+    | true -> lp() in
+  lp()
 
 
 
