0c21c7ca635a3384e0668a7ddbf85bd3e62a41d9
Justin Frank <justinpfrank@protonmail.com>
2022-11-08 20:58:17 -0500

    removed type annotations on variables

--- bin/gen_haskell.ml
+++ bin/gen_haskell.ml
@@ -1,5 +1,4 @@
 exception FoundHole
-exception IllTyped
 
 let haskell_string (prog : Exp.program) =
 
@@ -22,14 +21,15 @@ let haskell_string (prog : Exp.program) =
     | TyNdArrowExt (params, tyl') -> "(" ^ str_ty_params (prog.get_ty_params params) ^ str_ty tyl' ^ ")"
   in
 
-  let str_var var ty = "(" ^ Exp.Var.to_string var ^ "::" ^ str_ty ty ^ ")" in
+  (* let str_var var ty = "(" ^ Exp.Var.to_string var ^ "::" ^ str_ty ty ^ ")" in *)
+  let str_var = Exp.Var.to_string in
 
-  let str_params params ty_params =
+  let str_params params =
     match params with
     | [] -> "\\_ -> "
-    | _ -> List.fold_left2
-             (fun acc var ty -> "\\" ^ str_var var ty ^ " -> " ^ acc)
-             "" params ty_params
+    | _ -> List.fold_left
+             (fun acc var -> "\\" ^ str_var var ^ " -> " ^ acc)
+             "" params
   in
 
   let rec str_exp e =
@@ -62,27 +62,21 @@ let haskell_string (prog : Exp.program) =
                     | true -> "True"
                     | false -> "False")
     | Empty -> "([]::" ^ str_ty node.ty ^ ")"
-    | Cons (fst, rst) -> "(" ^ (str_exp fst) ^ " : " ^ (str_exp rst) ^ ")"
+    | Cons (fst, rst) -> "(" ^ str_exp fst ^ " : " ^ str_exp rst ^ ")"
     | Match (value, nil_case, (x_fst, x_rst, cons_case)) ->
-      "(case (" ^ (str_exp value) ^ "::" ^ str_ty (prog.get_exp value).ty ^ ") of {"
-      ^ "[] -> " ^ (str_exp nil_case) ^ "; "
-      ^ Exp.Var.to_string x_fst ^ ":" ^ Exp.Var.to_string x_rst ^ " -> " ^ (str_exp cons_case) ^ "})"
+      "(case " ^ str_exp value ^ " of {"
+      ^ "[] -> " ^ str_exp nil_case ^ "; "
+      ^ str_var x_fst ^ ":" ^ str_var x_rst ^ " -> " ^ str_exp cons_case ^ "})"
     | Let (var, rhs, body) ->
-      "(let " ^ str_var var (prog.get_exp rhs).ty
+      "(let " ^ str_var var
       ^ " = " ^ str_exp rhs
       ^ " in " ^ str_exp body ^ ")"
     | Lambda (params, body) ->
-      (match (prog.get_ty node.ty) with
-       | TyNdArrow (ty_params, _) ->
-         "(" ^ str_params params ty_params ^ str_exp body ^ ")"
-       | _ -> raise IllTyped)
+      "(" ^ str_params params ^ str_exp body ^ ")"
     | Call (func, args) ->
       "(" ^ str_exp func ^ str_args args ^ ")"
     | ExtLambda (params, body) ->
-      (match (prog.get_ty node.ty) with
-       | TyNdArrowExt (ty_params, _) ->
-         "(" ^ str_params (prog.get_params params) (prog.get_ty_params ty_params) ^ str_exp body ^ ")"
-       | _ -> raise IllTyped)
+      "(" ^ str_params (prog.get_params params) ^ str_exp body ^ ")"
     | ExtCall (func, args) ->
       "(" ^ str_exp func ^ str_args (prog.get_args args) ^ ")"
     | If (pred, thn, els) ->
